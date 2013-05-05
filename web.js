2041130b9d0e59fd5d337b50538746fb030b5bc1
sclark01 <sclark.tieche@gmail.com>
2013-05-05 00:05:01 -0400

    added welcome message

--- web.js
+++ web.js
@@ -2,3 +2,8 @@ var express = require ("express");
 var app = express();
 	app.use(express.bodyParser());
 	app.set('title','scorecenter');
+	
+app.get('/', function (req, res) {
+	res.set('Content-Type', 'text/html');
+	res.send("<h1>I'm Shea bitchs.");
+}
